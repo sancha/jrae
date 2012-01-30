1f78eb5407d9607aa7ba913aedaa7c779fcb618a
ssanjeev <ssanjeev@yongwhan-MacPro.(none)>
2012-01-30 11:01:51 -0800

    Removed matlab data that is mainly unused. And switched the RAE to also use the QNMinimizer.

--- src/rae/Main.java
+++ src/rae/Main.java
@@ -85,7 +85,7 @@ public class Main {
 		int DictionarySize = Dataset.Vocab.size();
 		int hiddenSize = EmbeddingSize, visibleSize = EmbeddingSize;
 		
-		LBFGSMinimizer minFunc = new LBFGSMinimizer(MaxIterations);
+		Minimizer<DifferentiableFunction> minFunc = new QNMinimizer(10, MaxIterations);
 
 		FineTunableTheta InitialTheta = new FineTunableTheta(EmbeddingSize,EmbeddingSize,CatSize,DictionarySize,true);
 		
@@ -103,7 +103,7 @@ public class Main {
 			{
 				RAECost = new RAECost(AlphaCat,CatSize,Beta,DictionarySize,hiddenSize,visibleSize,
 												Lambda,InitialTheta.We,trainingData,null,f);
-				double[] minTheta = minFunc.minimize(RAECost, InitialTheta.Theta, 1e-6);
+				double[] minTheta = minFunc.minimize(RAECost, 1e-6, InitialTheta.Theta);
 				tunedTheta = new FineTunableTheta(minTheta, hiddenSize,visibleSize, CatSize, DictionarySize);
 			}
 			else 
