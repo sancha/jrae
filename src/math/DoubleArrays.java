46198efbc497f160221cfa025edab5698b7093fb
Sanjeev Satheesh <ssanjeev@stanford.edu>
2012-01-30 11:50:08 -0800

    Brining back the serial version. The parallel version does not show major improvements. It could be the interface between the libraries that is causing this. So next, I will be testing
    i) The jblas interface
    ii) Propagation class that is now being forced to allocate and
    de-allocate a lot of memory.

--- src/math/DoubleArrays.java
+++ src/math/DoubleArrays.java
@@ -5,170 +5,193 @@ import java.util.Arrays;
 /**
  */
 public class DoubleArrays {
-  public static double[] clone(double[] x) {
-    double[] y = new double[x.length];
-    assign(y,x);
-    return y;
-  }
-  public static void assign(double[] y, double[] x) {
-    if (x.length != y.length) throw new RuntimeException("diff lengths: " + x.length + " " + y.length);
-    System.arraycopy(x, 0, y, 0, x.length);
-  }
-  public static double innerProduct(double[] x, double[] y) {
-    if (x.length != y.length) throw new RuntimeException("diff lengths: " + x.length + " " + y.length);
-    double result = 0.0;
-    for (int i = 0; i < x.length; i++) {
-      result += x[i]*y[i];
-    }
-    return result;
-  }
-  public static double[] addMultiples(double[] x, double xMultiplier, double[] y, double yMuliplier) {
-    if (x.length != y.length) throw new RuntimeException("diff lengths: " + x.length + " " + y.length);
-    double[] z = new double[x.length];
-    for (int i = 0; i < z.length; i++) {
-      z[i] = x[i]*xMultiplier + y[i]*yMuliplier;
-    }
-    return z;
-  }
-  public static double[] constantArray(double c, int length) {
-    double[] x = new double[length];
-    Arrays.fill(x, c);
-    return x;
-  }
-  public static double[] pointwiseMultiply(double[] x, double[] y) {
-    if (x.length != y.length) throw new RuntimeException("diff lengths: " + x.length + " " + y.length);
-    double[] z = new double[x.length];
-    for (int i = 0; i < z.length; i++) {
-      z[i] = x[i]*y[i];
-    }
-    return z;
-  }
-  public static String toString(double[] x) {
-    return toString(x, x.length);
-  }
-  public static String toString(double[] x, int length) {
-    StringBuffer sb = new StringBuffer();
-    sb.append("[");
-    for (int i = 0; i < SloppyMath.min(x.length, length); i++) {
-      sb.append(x[i]);
-      if (i + 1 < SloppyMath.min(x.length, length))
-        sb.append(", ");
-    }
-    sb.append("]");
-    return sb.toString();
-  }
-  public static void scale(double[] x, double s) {
-    if (s== 1.0) return;
-    for (int i = 0; i < x.length; i++) {
-      x[i] *= s;
-    }
-  }
+	public static double[] clone(double[] x) {
+		double[] y = new double[x.length];
+		assign(y, x);
+		return y;
+	}
 
-  public static double[] multiply(double[] x, double s) {
-    double[] result = new double[x.length];
-    if (s== 1.0) {
-      System.arraycopy(x, 0, result, 0, x.length);
-      return result;
-    }
-    for (int i = 0; i < x.length; i++) {
-      result[i] = x[i] * s;
-    }
-    return result;
-  }
+	public static void assign(double[] y, double[] x) {
+		if (x.length != y.length)
+			throw new RuntimeException("diff lengths: " + x.length + " "
+					+ y.length);
+		System.arraycopy(x, 0, y, 0, x.length);
+	}
 
-  public static int argMax(double[] v) {
-    int maxI = -1;
-    double maxV = Double.NEGATIVE_INFINITY;
-    for (int i = 0; i < v.length; i++) {
-      if (v[i] > maxV) {
-        maxV = v[i];
-        maxI = i;
-      }
-    }
-    return maxI;
-  }
+	public static double innerProduct(double[] x, double[] y) {
+		if (x.length != y.length)
+			throw new RuntimeException("diff lengths: " + x.length + " "
+					+ y.length);
+		double result = 0.0;
+		for (int i = 0; i < x.length; i++) {
+			result += x[i] * y[i];
+		}
+		return result;
+	}
 
-  public static double max(double[] v) {
-    double maxV = Double.NEGATIVE_INFINITY;
-    for (int i = 0; i < v.length; i++) {
-      if (v[i] > maxV) {
-        maxV = v[i];
-      }
-    }
-    return maxV;
-  }
+	public static double[] addMultiples(double[] x, double xMultiplier,
+			double[] y, double yMuliplier) {
+		if (x.length != y.length)
+			throw new RuntimeException("diff lengths: " + x.length + " "
+					+ y.length);
+		double[] z = new double[x.length];
+		for (int i = 0; i < z.length; i++) {
+			z[i] = x[i] * xMultiplier + y[i] * yMuliplier;
+		}
+		return z;
+	}
 
-  public static double[] add(double[] a, double b) {
-    double[] result = new double[a.length];
-    for (int i = 0; i < a.length; i++) {
-      double v = a[i];
-      result[i] = v+b;
-    }
-    return result;
-  }
+	public static double[] constantArray(double c, int length) {
+		double[] x = new double[length];
+		Arrays.fill(x, c);
+		return x;
+	}
 
-  public static double vectorLength(double[] x) {
-    return Math.sqrt(innerProduct(x, x));
-  }
-  
-  public static double[] addi(double[] x, double[] y) {
-    if (x.length != y.length) throw new RuntimeException("diff lengths: " + x.length + " " + y.length);
-    for (int i = 0; i < x.length; i++) {
-      x[i] = x[i] + y[i];
-    }
-    return x;
-  }
+	public static double[] pointwiseMultiply(double[] x, double[] y) {
+		if (x.length != y.length)
+			throw new RuntimeException("diff lengths: " + x.length + " "
+					+ y.length);
+		double[] z = new double[x.length];
+		for (int i = 0; i < z.length; i++) {
+			z[i] = x[i] * y[i];
+		}
+		return z;
+	}
 
-  public static double[] add(double[] x, double[] y) {
-    if (x.length != y.length) throw new RuntimeException("diff lengths: " + x.length + " " + y.length);
-    double[] result = new double[x.length];
-    for (int i = 0; i < x.length; i++) {
-      result[i] = x[i] + y[i];
-    }
-    return result;
-  }
+	public static String toString(double[] x) {
+		return toString(x, x.length);
+	}
 
-  public static double[] subtract(double[] x, double[] y) {
-    if (x.length != y.length) throw new RuntimeException("diff lengths: " + x.length + " " + y.length);
-    double[] result = new double[x.length];
-    for (int i = 0; i < x.length; i++) {
-      result[i] = x[i] - y[i];
-    }
-    return result;
-  }
+	public static String toString(double[] x, int length) {
+		StringBuffer sb = new StringBuffer();
+		sb.append("[");
+		for (int i = 0; i < SloppyMath.min(x.length, length); i++) {
+			sb.append(x[i]);
+			if (i + 1 < SloppyMath.min(x.length, length))
+				sb.append(", ");
+		}
+		sb.append("]");
+		return sb.toString();
+	}
 
-  public static void initialize(double[] x, double d) {
-    Arrays.fill(x, d);
-  }
-  public static void initialize(Object[] x, double d) {
-    for (int i = 0; i < x.length; i++) {
-      Object o = x[i];
-      if (o instanceof double[])
-        initialize((double[]) o, d);
-      else
-        initialize((Object[]) o, d);
-    }
-  }
-  
-  public static double total(double[] x)
-  {
-	double sum = 0;
-	for(int i=0; i<x.length; i++) sum+=x[i];
-	return sum;
-  }  
+	public static void scale(double[] x, double s) {
+		if (s == 1.0)
+			return;
+		for (int i = 0; i < x.length; i++) {
+			x[i] *= s;
+		}
+	}
 
-  public static void prettyPrint(double[] x)
-  {
-	  double sum = 0, abs_sum = 0;
-	  for(int i=0; i<x.length; i++)
-	  {
-		  sum += x[i];
-		  abs_sum += Math.abs(x[i]);
-		  System.out.printf("%.4f\n",x[i]);
-	  }
-	  System.out.println(sum);
-	  System.out.println(abs_sum);
-	  System.out.println();
-  }
-  
+	public static double[] multiply(double[] x, double s) {
+		double[] result = new double[x.length];
+		if (s == 1.0) {
+			System.arraycopy(x, 0, result, 0, x.length);
+			return result;
+		}
+		for (int i = 0; i < x.length; i++) {
+			result[i] = x[i] * s;
+		}
+		return result;
+	}
+
+	public static int argMax(double[] v) {
+		int maxI = -1;
+		double maxV = Double.NEGATIVE_INFINITY;
+		for (int i = 0; i < v.length; i++) {
+			if (v[i] > maxV) {
+				maxV = v[i];
+				maxI = i;
+			}
+		}
+		return maxI;
+	}
+
+	public static double max(double[] v) {
+		double maxV = Double.NEGATIVE_INFINITY;
+		for (int i = 0; i < v.length; i++) {
+			if (v[i] > maxV) {
+				maxV = v[i];
+			}
+		}
+		return maxV;
+	}
+
+	public static double[] add(double[] a, double b) {
+		double[] result = new double[a.length];
+		for (int i = 0; i < a.length; i++) {
+			double v = a[i];
+			result[i] = v + b;
+		}
+		return result;
+	}
+
+	public static double vectorLength(double[] x) {
+		return Math.sqrt(innerProduct(x, x));
+	}
+
+	public static double[] addi(double[] x, double[] y) {
+		if (x.length != y.length)
+			throw new RuntimeException("diff lengths: " + x.length + " "
+					+ y.length);
+		for (int i = 0; i < x.length; i++) {
+			x[i] = x[i] + y[i];
+		}
+		return x;
+	}
+
+	public static double[] add(double[] x, double[] y) {
+		if (x.length != y.length)
+			throw new RuntimeException("diff lengths: " + x.length + " "
+					+ y.length);
+		double[] result = new double[x.length];
+		for (int i = 0; i < x.length; i++) {
+			result[i] = x[i] + y[i];
+		}
+		return result;
+	}
+
+	public static double[] subtract(double[] x, double[] y) {
+		if (x.length != y.length)
+			throw new RuntimeException("diff lengths: " + x.length + " "
+					+ y.length);
+		double[] result = new double[x.length];
+		for (int i = 0; i < x.length; i++) {
+			result[i] = x[i] - y[i];
+		}
+		return result;
+	}
+
+	public static void initialize(double[] x, double d) {
+		Arrays.fill(x, d);
+	}
+
+	public static void initialize(Object[] x, double d) {
+		for (int i = 0; i < x.length; i++) {
+			Object o = x[i];
+			if (o instanceof double[])
+				initialize((double[]) o, d);
+			else
+				initialize((Object[]) o, d);
+		}
+	}
+
+	public static double total(double[] x) {
+		double sum = 0;
+		for (int i = 0; i < x.length; i++)
+			sum += x[i];
+		return sum;
+	}
+
+	public static void prettyPrint(double[] x) {
+		double sum = 0, abs_sum = 0;
+		for (int i = 0; i < x.length; i++) {
+			sum += x[i];
+			abs_sum += Math.abs(x[i]);
+			System.out.printf("%.4f\n", x[i]);
+		}
+		System.out.println(sum);
+		System.out.println(abs_sum);
+		System.out.println();
+	}
+
 }
--- src/rae/RAECostComputer.java
+++ src/rae/RAECostComputer.java
@@ -1,32 +1,30 @@
 package rae;
 
 import java.util.*;
-import java.util.concurrent.locks.*;
 
 import math.*;
 import org.jblas.*;
 import util.*;
 import classify.*;
-import parallel.*;
 
-public class RAECostComputer
-{
+public class RAECostComputer {
 	double AlphaCat, Beta;
 	double LambdaW, LambdaL, LambdaCat;
 	int CatSize, DictionaryLength, HiddenSize, NumExamples, num_nodes;
+	List<LabeledDatum<Integer, Integer>> DataCell;
 	DoubleMatrix WeOrig;
-	List<LabeledDatum<Integer,Integer>> DataCell;
 	RAEPropagation Propagator;
 	FloatMatrix FreqOrig;
 	DifferentiableMatrixFunction f;
-	Lock lock;
-	
-	double[] CalcGrad, Gradient;
+
+	double[] Gradient;
 	double cost;
 	Structure[] AllKids;
-	
-	public RAECostComputer(int CatSize, double AlphaCat, double Beta, int DictionaryLength, int HiddenSize
-				, List<LabeledDatum<Integer,Integer>> DataCell, FloatMatrix FreqOrig, DifferentiableMatrixFunction f){
+
+	public RAECostComputer(int CatSize, double AlphaCat, double Beta,
+			int DictionaryLength, int HiddenSize,
+			List<LabeledDatum<Integer, Integer>> DataCell,
+			FloatMatrix FreqOrig, DifferentiableMatrixFunction f) {
 		NumExamples = DataCell.size();
 		this.Beta = Beta;
 		this.CatSize = CatSize;
@@ -38,155 +36,160 @@ public class RAECostComputer
 		this.f = f;
 		num_nodes = 0;
 		AllKids = new Structure[NumExamples];
-		lock = new ReentrantLock();
 	}
-	
+
 	/**
-	 * This method is called by RAECost after building the Kids data
-	 * It is equivalent to calling computeCostAndGradRAE(allKids, theta2, 1 ....
+	 * This method is called by RAECost after building the Kids data It is
+	 * equivalent to calling computeCostAndGradRAE(allKids, theta2, 1 ....
+	 * @throws Exception
 	 */
-	public double Compute(final FineTunableTheta Theta, final double[] Lambda) throws Exception
-	{
+	public double Compute(FineTunableTheta Theta, double[] Lambda)
+			throws Exception {
 		setLambdas(Lambda);
-		
+		Propagator = new RAEPropagation(Theta, AlphaCat, Beta, HiddenSize,
+				DictionaryLength, f);
 		cost = 0;
 		num_nodes = 0;
-		CalcGrad = DoubleArrays.constantArray(0,Theta.Theta.length);
-		Propagator = new RAEPropagation(Theta,AlphaCat,Beta,HiddenSize,DictionaryLength,f);
-		
-		Parallel.For(DataCell, new Parallel.Operation<LabeledDatum<Integer,Integer>>() {
-			public void perform(int index, LabeledDatum<Integer,Integer> Data)
-			{
-				int[] WordIndices = ArraysHelper.getIntArray( Data.getFeatures() );
-				DoubleMatrix WordsEmbedded = Theta.We.getColumns(WordIndices);
-				//FloatMatrix Freq = FreqOrig.getColumns(Data);
-				
-				int CurrentLabel = Data.getLabel();
-				int SentenceLength = WordsEmbedded.columns;
-				
-				if(SentenceLength == 1)
-					return;
-				
-				
-				Tree tree = Propagator.ForwardPropagate(Theta, WordsEmbedded, FreqOrig, 
-										CurrentLabel, SentenceLength, AllKids[index]);
-				
-				double[] Gradient = Propagator.BackPropagate(tree, Theta, WordIndices).Theta;
-				
-				lock.lock();
-				{
-					cost += tree.TotalScore; 
-					num_nodes++;
-					DoubleArrays.addi(CalcGrad, Gradient);
-				}
-				lock.unlock();
-			}
-		});
-		
+
+		int CurrentDataIndex = 0;
+		for (LabeledDatum<Integer, Integer> Data : DataCell) {
+			int[] data = ArraysHelper.getIntArray(Data.getFeatures());
+			DoubleMatrix WordsEmbedded = Theta.We.getColumns(data);
+			// FloatMatrix Freq = FreqOrig.getColumns(Data);
+
+			int CurrentLabel = Data.getLabel();
+			int SentenceLength = WordsEmbedded.columns;
+
+			if (SentenceLength == 1)
+				continue;
+
+			Tree tree = Propagator.ForwardPropagate(Theta, WordsEmbedded,
+					FreqOrig, CurrentLabel, SentenceLength,
+					AllKids[CurrentDataIndex]);
+			cost += tree.TotalScore;
+			num_nodes++;
+
+			Propagator.BackPropagate(tree, Theta, data);
+			CurrentDataIndex++;
+		}
+
 		CalculateFineCosts(Theta);
-		return cost;		
+		return cost;
 	}
-	
+
 	/**
-	 * This method is called first by RAECost to build the Kids data
-	 * It is equivalent to calling computeCostAndGradRAE([], theta1, 0 ....
+	 * This method is called first by RAECost to build the Kids data It is
+	 * equivalent to calling computeCostAndGradRAE([], theta1, 0 ....
+	 * 
+	 * @param theta
+	 * @param AlphaCat
+	 * @param CatSize
+	 * @param WeOrig
+	 * @return
+	 * @throws Exception
 	 */
-	public double Compute(final Theta Theta, final double[] Lambda, final DoubleMatrix WeOrig) throws Exception
-	{
+	public double Compute(Theta Theta, double[] Lambda, DoubleMatrix WeOrig)
+			throws Exception {
 		setLambdas(Lambda);
-		
+		Propagator = new RAEPropagation(Theta, AlphaCat, Beta, HiddenSize,
+				DictionaryLength, f);
 		cost = 0;
 		num_nodes = 0;
-		CalcGrad = DoubleArrays.constantArray(0,Theta.Theta.length);
-		Propagator = new RAEPropagation(Theta,AlphaCat,Beta,HiddenSize,DictionaryLength,f);
-		
-		Parallel.For(DataCell, new Parallel.Operation<LabeledDatum<Integer,Integer>>() {
-			public void perform(int index, LabeledDatum<Integer,Integer> Data)
-			{
-				int[] WordIndices = ArraysHelper.getIntArray( Data.getFeatures() );
-				DoubleMatrix L = Theta.We.getColumns(WordIndices);
-				DoubleMatrix WordsEmbedded = (WeOrig.getColumns(WordIndices)).addi(L);
-				
-				int CurrentLabel = Data.getLabel();
-				int SentenceLength = WordsEmbedded.columns;
-				
-				if(SentenceLength == 1)
-					return;
-				
-				Tree tree = Propagator.ForwardPropagate(Theta, WordsEmbedded, FreqOrig, CurrentLabel, SentenceLength);
-				AllKids[ index ] = tree.structure;
-				
-				double[] Gradient = Propagator.BackPropagate(tree, Theta, WordIndices).Theta;
-				
-				lock.lock();
-				{	
-					cost += tree.TotalScore; 
-			        num_nodes += SentenceLength;
-			        DoubleArrays.addi(CalcGrad, Gradient);
-		        }
-				lock.unlock();	
-			}
-		});
-		
+		int CurrentDataIndex = 0;
+		for (LabeledDatum<Integer, Integer> Data : DataCell) 
+		{
+			int[] data = ArraysHelper.getIntArray(Data.getFeatures());
+			DoubleMatrix L = Theta.We.getColumns(data);
+			DoubleMatrix WordsEmbedded = (WeOrig.getColumns(data)).addi(L);
+			// FloatMatrix Freq = FreqOrig.getColumns(Data);
+
+			int CurrentLabel = Data.getLabel();
+			int SentenceLength = WordsEmbedded.columns;
+
+			if (SentenceLength == 1)
+				continue;
+
+			Tree tree = Propagator.ForwardPropagate(Theta, WordsEmbedded,
+					FreqOrig, CurrentLabel, SentenceLength);
+			AllKids[CurrentDataIndex] = tree.structure;
+			cost += tree.TotalScore;
+			num_nodes += SentenceLength;
+
+			Propagator.BackPropagate(tree, Theta, data);
+			CurrentDataIndex++;
+		}
 		num_nodes -= NumExamples;
 		CalculateCosts(Theta);
 		return cost;
 	}
-	
-	public double[] getGradient()
-	{
+
+	public double[] getGradient() {
 		return Gradient;
 	}
-	
-	public double getCost()
-	{
+
+	public double getCost() {
 		return cost;
 	}
-	
-	public Structure[] getKids()
-	{ 
+
+	public Structure[] getKids() {
 		return AllKids;
-	} 
-	
-	private void CalculateCosts(Theta Theta)
-	{
-		double WNormSquared = DoubleMatrixFunctions.SquaredNorm(Theta.W1) + DoubleMatrixFunctions.SquaredNorm(Theta.W2) +
-				DoubleMatrixFunctions.SquaredNorm(Theta.W3) + DoubleMatrixFunctions.SquaredNorm(Theta.W4);
-
-		cost = (1.0f/num_nodes)*cost + 0.5 * LambdaW * WNormSquared 
-							 + 0.5 * LambdaL * DoubleMatrixFunctions.SquaredNorm(Theta.We);
-		
-		DoubleMatrix b0 = DoubleMatrix.zeros(HiddenSize,1);
-		double[] WeightedGrad = (new Theta(Theta.W1.mul(LambdaW),Theta.W2.mul(LambdaW),Theta.W3.mul(LambdaW),
-		Theta.W4.mul(LambdaW),Theta.We.mul(LambdaL),b0,b0,b0)).Theta; 
-		
-		DoubleArrays.scale(CalcGrad, (1.0f/num_nodes));
-		Gradient = DoubleArrays.add(CalcGrad, WeightedGrad);		
 	}
 
-	private void CalculateFineCosts(FineTunableTheta Theta)
-	{
-		double WNormSquared = DoubleMatrixFunctions.SquaredNorm(Theta.W1) + DoubleMatrixFunctions.SquaredNorm(Theta.W2) +
-				DoubleMatrixFunctions.SquaredNorm(Theta.W3) + DoubleMatrixFunctions.SquaredNorm(Theta.W4);
-		
-		cost = (1.0f/num_nodes)*cost + 0.5 * LambdaW * WNormSquared
-						+ 0.5 * LambdaL * DoubleMatrixFunctions.SquaredNorm(Theta.We)
-						+ 0.5 * LambdaCat * DoubleMatrixFunctions.SquaredNorm(Theta.Wcat);
-		
-		/** WNormSquared, DoubleMatrixFunctions.SquaredNorm(Theta.We), DoubleMatrixFunctions.SquaredNorm(Theta.Wcat)); **/
-		DoubleMatrix bcat0 = DoubleMatrix.zeros(CatSize,1);
-		DoubleMatrix b0 = DoubleMatrix.zeros(HiddenSize,1);
-		double[] WeightedGrad = (new FineTunableTheta(Theta.W1.mul(LambdaW),Theta.W2.mul(LambdaW),Theta.W3.mul(LambdaW),
-		Theta.W4.mul(LambdaW),Theta.Wcat.mul(LambdaCat),Theta.We.mul(LambdaL),b0,b0,b0,bcat0)).Theta; 
-		
-		DoubleArrays.scale(CalcGrad, (1.0f/num_nodes));
-		Gradient = DoubleArrays.add(CalcGrad, WeightedGrad);		
+	private void CalculateCosts(Theta Theta) {
+		double WNormSquared = DoubleMatrixFunctions.SquaredNorm(Theta.W1)
+				+ DoubleMatrixFunctions.SquaredNorm(Theta.W2)
+				+ DoubleMatrixFunctions.SquaredNorm(Theta.W3)
+				+ DoubleMatrixFunctions.SquaredNorm(Theta.W4);
+
+		cost = (1.0f / num_nodes) * cost + 0.5 * LambdaW * WNormSquared + 0.5
+				* LambdaL * DoubleMatrixFunctions.SquaredNorm(Theta.We);
+
+		DoubleMatrix b0 = DoubleMatrix.zeros(HiddenSize, 1);
+		double[] WeightedGrad = (new Theta(Theta.W1.mul(LambdaW),
+				Theta.W2.mul(LambdaW), Theta.W3.mul(LambdaW),
+				Theta.W4.mul(LambdaW), Theta.We.mul(LambdaL), b0, b0, b0)).Theta;
+
+		double[] CalcGrad = (new Theta(Propagator.GW1, Propagator.GW2,
+				Propagator.GW3, Propagator.GW4, Propagator.GWe_total,
+				Propagator.Gb1, Propagator.Gb2, Propagator.Gb3)).Theta;
+
+		DoubleArrays.scale(CalcGrad, (1.0f / num_nodes));
+		Gradient = DoubleArrays.add(CalcGrad, WeightedGrad);
 	}
 
-	private void setLambdas(double[] Lambda)
-	{
-		LambdaW = Lambda[0];	
-		LambdaL = Lambda[1]; 	
-		LambdaCat = Lambda[2];		
+	private void CalculateFineCosts(FineTunableTheta Theta) {
+		double WNormSquared = DoubleMatrixFunctions.SquaredNorm(Theta.W1)
+				+ DoubleMatrixFunctions.SquaredNorm(Theta.W2)
+				+ DoubleMatrixFunctions.SquaredNorm(Theta.W3)
+				+ DoubleMatrixFunctions.SquaredNorm(Theta.W4);
+
+		cost = (1.0f / num_nodes) * cost + 0.5 * LambdaW * WNormSquared + 0.5
+				* LambdaL * DoubleMatrixFunctions.SquaredNorm(Theta.We) + 0.5
+				* LambdaCat * DoubleMatrixFunctions.SquaredNorm(Theta.Wcat);
+
+		/**
+		 * WNormSquared, DoubleMatrixFunctions.SquaredNorm(Theta.We),
+		 * DoubleMatrixFunctions.SquaredNorm(Theta.Wcat));
+		 **/
+		DoubleMatrix bcat0 = DoubleMatrix.zeros(CatSize, 1);
+		DoubleMatrix b0 = DoubleMatrix.zeros(HiddenSize, 1);
+		double[] WeightedGrad = (new FineTunableTheta(Theta.W1.mul(LambdaW),
+				Theta.W2.mul(LambdaW), Theta.W3.mul(LambdaW),
+				Theta.W4.mul(LambdaW), Theta.Wcat.mul(LambdaCat),
+				Theta.We.mul(LambdaL), b0, b0, b0, bcat0)).Theta;
+
+		double[] CalcGrad = (new FineTunableTheta(Propagator.GW1,
+				Propagator.GW2, Propagator.GW3, Propagator.GW4,
+				Propagator.GWCat, Propagator.GWe_total, Propagator.Gb1,
+				Propagator.Gb2, Propagator.Gb3, Propagator.Gbcat)).Theta;
+
+		DoubleArrays.scale(CalcGrad, (1.0f / num_nodes));
+		Gradient = DoubleArrays.add(CalcGrad, WeightedGrad);
+	}
+
+	private void setLambdas(double[] Lambda) {
+		LambdaW = Lambda[0];
+		LambdaL = Lambda[1];
+		LambdaCat = Lambda[2];
 	}
 }
